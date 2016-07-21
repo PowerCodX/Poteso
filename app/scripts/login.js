8206fb26ca30f4b9c339b16635611e062395c8a0
Victor Pogor <pogor.victor@hotmail.com>
2016-07-21 12:55:18 +0300

    Added Preloader

---
+++ app/scripts/login.js
@@ -0,0 +1,11 @@
+/**
+ * Created by pogor on 21-Jul-16.
+ */
+$(document).ready(function() {
+
+  setTimeout(function(){
+    $('#preloader').fadeOut();
+    $('.preloader-wrapper').removeClass('active');
+  }, 1000);
+
+});
